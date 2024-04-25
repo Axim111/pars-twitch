9a60110a2c782b5b999608093cc7c38c0dcf1776
Axim <makseropolov@gmail.com>
2024-04-25 09:46:47 +0500

    +csv +- scroll

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import puppeteer from "puppeteer"
 import fs from "fs"
 
+
+
+//для скрола
+const scrollPageToBottom = async () => {
+  await page.evaluate(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+  await new Promise(resolve => setTimeout(resolve, 1000));
+};
+
+
+
 let urlList = fs.readFileSync("url.txt", "utf8", async (err, text) => {
   if (err) throw new Error("Что-то пошло не так!");
 
@@ -20,36 +32,76 @@ const browser = await puppeteer.launch(
   }
 );
 const page = await browser.newPage();
-for (let url of urlMass) {
-
-
-  await page.goto(url);
-
-
-  await page.waitForSelector(".tw-image", { visible: true })
-
-  const cards = await page.$$(".tw-link > h2")
-  // page.on("request",(req)=>{
-  //   console.log(req.url)
-  // })
 
 
 
+//ищет названия карточке по txt(txt в список выше)
+// for (let url of urlMass) {
 
-  for (let item of cards) {
-    console.log(await item.evaluate((node) => {
-      return node.innerText
-    }))
-  }
 
-  //csv file
+// await page.goto(url);
 
-  // let link = "https://www.twitch.tv/directory"
-  // await page.goto(link);
-  // const itemsOfCard = await page.$$("game-card__link")
-  // for (let i in itemsOfCard) {
-  //   console.log(await i.evaluate((node) => node.getAttribute("game-card__link")))
-  // }
+
+// await page.waitForSelector(".tw-image", { visible: true })
+
+// const cards = await page.$$(".tw-link > h2")
+
+
+// for (let item of cards) {
+//   console.log(await item.evaluate((node) => {
+//     return node.innerText
+//   }))
+// }
+// }
+
+
+//csv file
+
+
+//база
+let link = "https://www.twitch.tv/directory"
+await page.goto(link);
+
+
+
+const itemsOfCard = await page.$$(".tw-card > div")
+console.log(itemsOfCard)
+
+
+//очистка перед записью 
+fs.writeFile("stream.csv", 'gameName, gameRef, online\n',
+  function (error) {
+    if (error) return console.log(error); // если возникла ошибка
+
+    console.log("Запись файла завершена");
+  });
+
+
+
+//сама запись
+for (let i of itemsOfCard) {
+
+  var gameName = null, gaemeImg = null,
+    gameRef = null, online = null, streamers = null
+
+
+  gameName = await i.evaluate((node) => node.querySelector("div  > div > div > span >a").innerText)
+  gameRef = await i.evaluate((node) => node.querySelector("a").getAttribute("href"))
+  online = await i.evaluate((node) => node.querySelector(".tw-card-body > p > a").innerText)
+  console.log(gameName, online, gameRef)
+
+
+
+  fs.appendFile("stream.csv", `${gameName.replace(/,/g, ".")},
+     ${gameRef.replace(/,/g, ".")},
+      ${online.replace(/,/g, ".")}\n`,
+    function (error) {
+      if (error) return console.log(error); // если возникла ошибка
+
+      console.log("Запись файла завершена");
+    });
 
 
 }
+
+
